f9b93e2044e365abeaa8dc3eea1e22861c260faa
JeJin-Lee <53627651+JeJin-Lee@users.noreply.github.com>
2019-08-18 19:48:12 +0900

    Create bubble_sort.c

---
+++ bubble_sort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void bubble_sort(int arr[], int count){
+  int temp;
+  for(int i=0; i<count; i++){
+    for(int j=0; j<count-1; j++){
+      if(arr[j] > arr[j+1])
+      {
+        temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+        }
+     }
+   }
+}
+
+   
